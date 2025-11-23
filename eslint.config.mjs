406264cfd31eb55362a10d4e1d0ab160f5559df9
my-frigedai <auos.05.04-conan@au.com>
2025-11-23 18:39:21 +0900

    relax eslint rules for build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import next from "eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ["**/dist/**", "**/.next/**"],
+  },
+  ...next(),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "jsx-a11y/role-supports-aria-props": "warn",
+      "@next/next/no-html-link-for-pages": "warn",
+      "react-hooks/exhaustive-deps": "warn"
+    },
   },
 ];
-
-export default eslintConfig;
